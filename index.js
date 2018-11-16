156a379b156de51e9a44ef46695ad5a815fd80f7
sixone <931039665@qq.com>
2018-11-16 20:34:09 +0800

    信息更新

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+	// 模版标识
+	name: 'house-three',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		},
+		{
+			name: '产品',
+			// 路径
+			path: 'product',
+			// 预览图
+			thumbnail: 'product.png',
+		},
+		{
+			name: '新闻',
+			// 路径
+			path: 'news',
+			// 预览图
+			thumbnail: 'news.png',
+		},
+		{
+			name: '团队',
+			// 路径
+			path: 'team',
+			// 预览图
+			thumbnail: 'team.png',
+		},
+		{
+			name: '服务',
+			// 路径
+			path: 'service',
+			// 预览图
+			thumbnail: 'service.png',
+		}
+	]
+}
